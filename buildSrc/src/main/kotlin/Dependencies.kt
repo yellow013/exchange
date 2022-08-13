d4bd390c70814f45346830c11760f94a6edd804d
eliquinox <eliquinox@outlook.com>
2022-08-13 16:39:50 +0100

    Bump aeronic

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,7 +1,7 @@
 object Dependencies
 {
     const val aeron = "io.aeron:aeron-all:1.38.1"
-    const val aeronic = "io.aeronic:aeronic:0.0.16"
+    const val aeronic = "io.aeronic:aeronic:0.0.17"
     const val agrona = "org.agrona:agrona:1.8.0"
     const val apacheConfiguration = "org.apache.commons:commons-configuration2:2.7"
     const val apacheBeanUtils = "commons-beanutils:commons-beanutils:1.9.4"
